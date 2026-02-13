d3de63d1acadd3d471397b05e638be55dd24f865
nitinsengar007 <155714711+nitinsengar007@users.noreply.github.com>
2026-02-13 15:54:08 +0530

    Add branching concept explanation in palindrone.c
    Added explanation about branching concept in DevOps.

---
+++ palindrone.c
@@ -0,0 +1,4 @@
+I'm learning the branching concept in Devops.
+  branching is the process can be done by a developer to dont directly add any changes in the main branch first they can add new branh and then send pull/review request 
+  to any person for reviewing the changes.
+onces the changes is done then if the branch is delete then the other branch is store the same data cannot be delete.
